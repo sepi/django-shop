334c3f34f8a049c33e0e3faa1bd34c1aa51f29bb
Raffael Mancini <raffael@mancini.lu>
2023-11-13 17:49:34 +0100

    Can use native JSONField now (since Django 3.1)

--- shop/models/fields.py
+++ shop/models/fields.py
@@ -1,6 +1,7 @@
 import enum
 from django.conf import settings
 from django.db import models
+from django.db.models import JSONField as _JSONField
 from django.utils.encoding import force_str
 from django.utils.translation import gettext_lazy as _
 
@@ -9,12 +10,6 @@ postgresql_engine_names = [
     'django.db.backends.postgresql',
     'django.db.backends.postgresql_psycopg2',
 ]
-
-if settings.DATABASES['default']['ENGINE'] in postgresql_engine_names:
-    from django.contrib.postgres.fields import JSONField as _JSONField
-else:
-    from jsonfield.fields import JSONField as _JSONField
-
 
 class JSONField(_JSONField):
     def __init__(self, *args, **kwargs):
